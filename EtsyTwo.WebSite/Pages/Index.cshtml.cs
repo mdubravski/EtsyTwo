f2339b824708b6120acf46af5506d911339effd8
mdubravski <mdubravski@gmail.com>
2022-11-21 07:33:04 -0500

    add files

---
+++ EtsyTwo.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using EtsyTwo.WebSite.Models;
+using EtsyTwo.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtsyTwo.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductService _productService;
+        public IEnumerable<Product> _products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger, 
+            JsonFileProductService jsonFileProductService)
+        {
+            _logger = logger;
+            _productService = jsonFileProductService;
+        }
+
+        public void OnGet()
+        {
+             _products = _productService.GetProducts();
+        }
+    }
+}
